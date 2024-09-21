6615c613b3b46d9091949941636daadf85a2cd95
Jonathan Koranteng <kor22002@byui.edu>
2024-09-21 08:13:11 +0000

    Individual broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  const cart = getLocalStorage('so-cart') || []; //get existing items
+  cart.push(product);  //add new item
+  setLocalStorage('so-cart', cart); //update local storage
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
