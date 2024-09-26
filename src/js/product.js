941804dcae2a45a7436c3c8d5a6c2b7b6bf82ec9
NaNajli <nadurodriguez2@gmail.com>
2024-09-26 19:09:58 -0300

    Dynamic product detail pages

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,10 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { getParams } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
+/*function addProductToCart(product) {
   const cart = getLocalStorage('so-cart') || []; //get existing items
   cart.push(product); //add new item
   setLocalStorage('so-cart', cart); //update local storage
@@ -17,4 +18,10 @@ async function addToCartHandler(e) {
 // add listener to Add to Cart button
 document
   .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .addEventListener('click', addToCartHandler);*/
+
+const productId = getParams('product');
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+//console.log(dataSource.findProductById(productId));
