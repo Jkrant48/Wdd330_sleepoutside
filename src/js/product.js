a37c6314158dbf6bc00697f2673cf24c8d5aa288
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-09-26 18:15:23 -0700

    my work through team exercise

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,34 @@
-import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductData.mjs';
 
+// get json path for catetory, set up instance
 const dataSource = new ProductData('tents');
+// console.log(dataSource);
+//get id from url search string
+const productId = getParam('product');
+// console.log(productId);
+// console.log(dataSource.findProductById(productId));
+//set up instance with json source and product id,
+const product = new ProductDetails(productId, dataSource);
+//product.init();
+console.log(product);
 
-function addProductToCart(product) {
-  const cart = getLocalStorage('so-cart') || []; //get existing items
-  cart.push(product); //add new item
-  setLocalStorage('so-cart', cart); //update local storage
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// move code to ProductDetails.mjs file and initialize in product above
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// function addProductToCart(product) {
+//   const cart = getLocalStorage('so-cart') || []; //get existing items
+//   cart.push(product); //add new item
+//   setLocalStorage('so-cart', cart); //update local storage
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   console.log(dataSource.findProductById(e.target.dataset.id));
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,16 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener('click', callback);
 }
+
+// a function to get a parameter from the URL when we need to.
+export function getParam(param) {
+  //grab the query string
+  const queryString = window.location.search;
+  //parse the query string's parameter
+  const urlParams = new URLSearchParams(queryString);
+  // return the first value associated with the given search parameter:
+  const product = urlParams.get(param);
+  //return result
+  return product;
+
+}
