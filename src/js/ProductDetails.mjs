8fdd880ab0078c193f20e3026dd424f6d046bb8a
Wiselinda <zid2400@byui.edu>
2024-09-28 19:36:55 +0300

    add discount

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,29 +1,36 @@
 import { setLocalStorage } from './utils.mjs';
 
 function productDetailstemplate(product) {
+    let discountMessage = '';
+    if (product.SuggestedRetailPrice > product.FinalPrice) {
+      const discountAmount = product.SuggestedRetailPrice - product.FinalPrice;
+      const discountPercentage = ((discountAmount / product.SuggestedRetailPrice) * 100).toFixed(2);
+      discountMessage = `<p class="product__discount">Save $${discountAmount} (${discountPercentage}% off)</p>`;
+    }
+  
     return `<section class="product-detail">
-        <h3>${product.Brand.Name}</h3>
-
-        <h2 class="divider">${product.NameWithoutBrand}</h2>
-
-        <img
-          class="divider"
-          src= ${product.Image}
-          alt=${product.Name}
-        />
-
-        <p class="product-card__price">${product.ListPrice}</p>
-
-        <p class="product__color">${product.Colors[0].ColorName}</p>
-
-        <p class="product__description">${product.DescriptionHtmlSimple}</p>
-
-        <div class="product-detail__add">
-          <button id="addToCart" data-id=${product.Id}>Add to Cart</button>
-        </div>
-        </section>`
-}
-
+      <h3>${product.Brand.Name}</h3>
+  
+      <h2 class="divider">${product.NameWithoutBrand}</h2>
+  
+      <img
+        class="divider"
+        src=${product.Image} 
+        alt=${product.Name}
+      />
+  
+      <p class="product-card__price">$${product.FinalPrice}</p>
+      ${discountMessage}
+      
+      <p class="product__color">${product.Colors[0].ColorName}</p>
+  
+      <p class="product__description">${product.DescriptionHtmlSimple}</p>
+  
+      <div class="product-detail__add">
+        <button id="addToCart" data-id=${product.Id}>Add to Cart</button>
+      </div>
+    </section>`;
+  }
 
 export default class ProductDetails{
 
