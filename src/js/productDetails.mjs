1e68e98826dcb36ce969b9ea784a6232df4c3551
Wiselinda <zid2400@byui.edu>
2024-09-29 21:55:02 +0300

    fix

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -14,24 +14,24 @@ function productDetailsTemplate(product) {
     </p>`;
   }
 
-  return `<section class="product-detail"> 
-      <h3>${product.Brand.Name}</h3>
-      <h2 class="divider">${product.NameWithoutBrand}</h2>
-      <img
-        class="divider"
+    return `<section class="product-detail">
+        <h3>${product.Brand.Name}</h3>
+        <h2 class="divider">${product.NameWithoutBrand}</h2>
+        <img
+          class="divider"
         src="${product.Image}"
         alt="${product.NameWithoutBrand}"
-      />
+        />
       ${discountDisplay}
       <p class="product-card__price">Original Price: <s>$${originalPrice.toFixed(2)}</s></p>
       <p class="product-card__price">Discounted Price: $${discountedPrice.toFixed(2)}</p>
-      <p class="product__color">${product.Colors[0].ColorName}</p>
+        <p class="product__color">${product.Colors[0].ColorName}</p>
       <p class="product__description">
         ${product.DescriptionHtmlSimple}
       </p>
-      <div class="product-detail__add">
+        <div class="product-detail__add">
         <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-      </div>
+        </div>
     </section>`;
 }
 
