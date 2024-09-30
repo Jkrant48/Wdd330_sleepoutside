6dca2c0c45307551f2d0f9e80ef7970964ccd751
Wiselinda <zid2400@byui.edu>
2024-09-30 19:44:17 +0300

    fix all changhes

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -46,27 +46,27 @@ export default class ProductDetails {
     // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
     //render product details out into html
-    this.renderProductDetails("main");
+    this.renderProductDetails('main');
     //add event handlers
     document
-      .getElementById("addToCart")
-      .addEventListener("click", this.addToCart()); //I removed the bind
+      .getElementById('addToCart')
+      .addEventListener('click', this.addToCart()); //I removed the bind
   }
 
   addToCart() {
-    let cart = getLocalStorage("so-cart") || [];
+    let cart = getLocalStorage('so-cart') || [];
     if (!Array.isArray(cart)) {
       cart = [];
     }
     console.log(cart);
     cart.push(this.product);
-    setLocalStorage("so-cart", cart);
+    setLocalStorage('so-cart', cart);
   }
 
   renderProductDetails(selector) {
     const element = document.querySelector(selector);
     element.insertAdjacentHTML(
-      "afterBegin",
+      'afterBegin',
       productDetailsTemplate(this.product),
     );
   }
