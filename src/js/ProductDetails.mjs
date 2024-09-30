5160acb22484d95343877f0c63789791a21a6987
Wiselinda <zid2400@byui.edu>
2024-09-30 15:22:07 +0300

    fix my issue

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -62,7 +62,7 @@ export default class ProductDetails{
     renderProductDetails(selector){
         const element = document.querySelector(selector);
         element.insertAdjacentHTML(
-            'Afterbegin', productDetailstemplate(this.product)
+            'Afterbegin', productDetailsTemplate(this.product)
         );
     }
 
