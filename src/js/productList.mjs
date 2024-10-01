260be575156cbd3e777c4f50b9da7cd3fe8474af
NaNajli <nadurodriguez2@gmail.com>
2024-10-01 14:18:29 -0300

    discount improving

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -10,7 +10,7 @@ function productCardTemplate(product) {
               <h3 class="card__brand">${product.Brand.Name}</h3>
               <h2 class="card__name">${product.Name}</h2>
               <p class="product-card__price">$${product.ListPrice}</p>
-              <p class = "discount" style="visibility: hidden"> Discount</p></a
+              <div class="discount"</div></a
             >
           </li>`;
 }
@@ -26,31 +26,29 @@ export default class ProductListing {
     //get data
     const list = await this.dataSource.getData(this.productCategory);
     console.log(list);
-   this.renderList(this.filterPoductList(list)); 
+    this.renderList(this.filterPoductList(list));  
+    this.discounted()
   }
 
   filterPoductList(list) {
-    const desiredIds = ["880RR", "985RF", "985PR", "344YJ"];
+    const desiredIds = ['880RR', '985RF', '985PR', '344YJ'];
     return list.filter((product) => desiredIds.includes(product.Id));
   }
 
   renderList(list) {
-    console.log("HtmlElement:", this.HtmlElement);
+    console.log('HtmlElement:', this.HtmlElement);
 
     renderListWithTemplate(productCardTemplate, this.HtmlElement, list);
   }
-  discounted (product){ 
-    const retail = product.SuggestedRetailPrice;
-    const price = product.FinalPrice;
-    if (price < retail){ 
-      let p = document.getElementById('.discount');
-      p.style.visibility = 'visible'
-  }
-  else{
-    let p = document.getElementById('.discount');
-    p.style.visibility = 'hidden'
-    
-  }
+ 
+discounted (product){ 
+   const retail = product.SuggestedRetailPrice;
+   const price = product.FinalPrice;
+  if (price > retail){ 
+    const sing = document.createElement('p');
+    sing.textContent = `Discount`;
+    document.querySelector('.discount').appendChild(sing)   
+}
+}
+}
 
-}
-}
