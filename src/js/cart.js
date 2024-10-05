1d4305bd06df74b0c997824a7844d8d71fa49357
kkmorrisfam <kkmorrisfam@hotmail.com>
2024-10-04 23:17:50 -0700

    import ShoppingCart fix

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,11 @@
 import { loadHeaderFooter } from './utils.mjs';
+import ShoppingCart from './shoppingCart.mjs';
 
 //runs on cart/index.html
 
-const cartFooterDOM = document.querySelector('.cart-footer');
-const cartTotalDOM = document.querySelector('.cart-total');
-const cartProductListDOM = document.querySelector('.product-list');
+// const cartFooterDOM = document.querySelector('.cart-footer');
+// const cartTotalDOM = document.querySelector('.cart-total');
+// const cartProductListDOM = document.querySelector('.product-list');
 
 loadHeaderFooter();
 
